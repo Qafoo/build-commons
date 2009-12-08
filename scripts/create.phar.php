32502435878b8e76fc9907232d8a3512f196eba4
Manuel Pichler <mapi@manuel-pichler.de>
2009-12-08 20:26:43 +0000

    # Common files added.
    git-svn-id: svn://svn.xplib.de/build-commons/trunk@2 6a98d669-40ad-46bf-905d-8dba7202fe92

---
+++ scripts/create.phar.php
@@ -0,0 +1,26 @@
+#!/usr/bin/env php
+<?php
+
+if ( count( $argv ) < 4 )
+{
+    echo 'Usage: create.phar.php <project> <output-file> <input-dir> [<bootstrap>]', PHP_EOL;
+    exit( 1 );
+}
+
+$project = $argv[1] . '.phar';
+$output  = $argv[2];
+$input   = realpath( $argv[3] );
+
+if ( isset( $argv[4] ) && trim( $argv[4] ) !== '' )
+{
+    $stub = file_get_contents( $argv[4] );
+}
+else
+{
+    $stub = '<?php Phar::mapPhar("${archive.alias}"); __HALT_COMPILER(); ?>';
+}
+
+$phar = new Phar( $output, 0, $project );
+$phar->buildFromDirectory( $input );
+$phar->setStub( str_replace( '${archive.alias}', $project, $stub ) );
+$phar->compressFiles( Phar::GZ );
