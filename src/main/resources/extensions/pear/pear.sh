3a746e50d5cefa1a3a6959cba0b5d9ea6de23bd8
Manuel Pichler <mapi@manuel-pichler.de>
2011-05-18 21:12:14 +0200

    Compress *.phar archive and add signature.

--- src/main/resources/extensions/pear/pear.sh
+++ src/main/resources/extensions/pear/pear.sh
@@ -16,23 +16,20 @@ pear -c $PEARDIR/pear.conf install --alldeps PEAR
 
 sed "s/(.*'@'.'include_path'.'@'.*)/(false)/" $PEARCMD > $PEARCMD.temp
 mv $PEARCMD.temp $PEARCMD
-rm $PEARCMD.temp
 
 sed "s/(.*'@'.'include_path'.'@'.*)/(false)/" $PECLCMD > $PECLCMD.temp
 mv $PECLCMD.temp $PECLCMD
-rm $PECLCMD.temp
 
 echo '<?php ob_start(); set_include_path("." . PATH_SEPARATOR . __DIR__ . "/php"); include "php/pearcmd.php";' > $PEARDIR/pear/index.php
 
 cd $PEARDIR
-#exit;
 
-rm -f pear.phar
+php -d phar.readonly=0 \
+    `which phar` pack  \
+    -f pear.phar       \
+    -a pear            \
+    -c bz2             \
+    -h sha512          \
+    -i 'pear\/(index|php\/(Ar|Co|OS|PE|St|Sy|XM|pe))' pear
 
-php -d phar.readonly=0 `which phar` pack -f pear.phar -a pear -i 'pear\/(index|php\/(Ar|Co|OS|PE|St|Sy|XM|pe))' pear
 php -d include_path=/tmp pear.phar
-
-
-
-
-
