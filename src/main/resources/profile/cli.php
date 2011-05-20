1bb39ecae14dd50fcac21f0301dff30a3d070ae7
Manuel Pichler <mapi@manuel-pichler.de>
2011-05-21 00:03:20 +0200

    Basic profiling build step added.

---
+++ src/main/resources/profile/cli.php
@@ -0,0 +1,17 @@
+<?php
+function __ant_build_commons_profile($startTime)
+{
+    ob_end_clean();
+    echo "Execution time (s),Memory usage (Mb)", PHP_EOL;
+    printf(
+        "%d,%.2f",
+        (time() - $startTime),
+        (memory_get_peak_usage() / 1024 / 1024),
+        PHP_EOL
+    );
+}
+
+register_shutdown_function('__ant_build_commons_profile', time());
+
+ob_start();
+include $argv[0];
