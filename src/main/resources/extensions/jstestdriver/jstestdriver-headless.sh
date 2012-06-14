fa213349556e2d2c637da2d90481cfbc7b9f9a11
Manuel Pichler <mapi@manuel-pichler.de>
2012-06-14 14:36:54 +0200

    Try to run jsTestDriver headless

---
+++ src/main/resources/extensions/jstestdriver/jstestdriver-headless.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# directory to write output XML (if this doesn't exist, the results will not be generated!)
+JSTD_JAR="$1"
+OUTPUT_DIR="$2"
+JSTD_PORT="$3"
+JSTD_BROWSER="$4"
+JSTD_CONFIG="$5"
+
+XVFB=`which Xvfb`
+if [ "$?" -eq 1 ];
+then
+    echo "Xvfb not found."
+    exit 1
+fi
+
+$XVFB :99 -ac &    # launch virtual framebuffer into the background
+PID_XVFB="$!"      # take the process ID
+export DISPLAY=:99 # set display to use that of the xvfb
+
+# run the tests
+DISPLAY=:99 java -jar "${JSTD_JAR}" --config "${JSTD_CONFIG}" --port "${JSTD_PORT}" --browser "${JSTD_BROWSER}" --tests all --testOutput "${OUTPUT_DIR}"
+
+kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
+echo "Done."
