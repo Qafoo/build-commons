368bea310912f54e4378d6a5bb5971fa974bc85b
Jakob Westhoff <jakob@westhoffswelt.de>
2013-03-29 16:51:31 +0100

    Implement: Really simple phpunit call to execute some tests

---
+++ Experiments/tests/php/fooTest.php
@@ -0,0 +1,13 @@
+<?php
+class FooTest extends  \PHPUnit_Framework_TestCase
+{
+    public function testBlub()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testBlab()
+    {
+        $this->assertTrue(false);
+    }
+}
